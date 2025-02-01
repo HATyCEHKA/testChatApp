d30b55c1e0ba4e9121b57d4950e2fece768e236b
HATyCEHKA <HATyCEHKA@gmail.com>
2025-02-01 20:47:26 +0300

    Улучшено отображение сообщений
    В сообщении добавлено отображение имени пользователя и локализованной даты.

--- src/app/home/messages/messages.component.ts
+++ src/app/home/messages/messages.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import {TranslateModule} from "@ngx-translate/core";
 import {Observable} from "rxjs";
 import {Message} from "../../model/chat";
+import {User} from "../../model/user";
 
 @Component({
   selector: 'messages',
@@ -18,5 +19,21 @@ export class MessagesComponent implements OnInit {
   ngOnInit() { }
 
   Messages: InputSignal<Message[] | undefined> = input<Message[]>();
+  Users: InputSignal<User[] | null | undefined > = input<User[]|null>();
+
+
+  protected getUserName(id:Number){
+    let users = this.Users();
+    if(!users) return id;
+    let user =  users.find(u=>u.id==id);
+    return user? user.username : id;
+  }
+
+  protected getLocalTime(timeString:string){
+    let date = Date.parse(timeString);
+    if(!isNaN(date))
+      return  new Date(date).toLocaleString();
+    return timeString;
+  }
 
 }
